636ff5ee4e841aa78c5e617203b7c3767a64b3b0
Yevhen Ivanov <hxrizxnqq@gmail.com>
2025-07-22 23:25:22 +0200

    Refactured the total command.

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -5,37 +5,38 @@ import { summaryCommandHandler } from "@/utils/commands/summary";
 import { sendMessage, addToSum } from "@/utils/telegram";
 
 export const config = {
-  maxDuration: 60,
+	maxDuration: 60,
 };
 
 export default async function handler(req, res) {
-  if (req.method=="POST") {
-    const chatId = req.body.message.chat.id;
-    const text = req.body.message.text?.trim();
-    console.log("ChatID", chatId);
-    console.log("text", text);
-    
-    if (!text) {
-      res.status(200).send("OK");
-      return;
-    }
-    
-    if (text.startsWith("/start") || text.startsWith("/help") ) {
-      await helpCommand(chatId)
-    }
-    else if (text === "/итог") {
-      await summaryCommandHandler(chatId);
-    }
-    else {
-      // Попробуем обработать как число
-      const numberAdded = await addToSum(chatId, text);
-      if (!numberAdded) {
-        await sendMessage(chatId, "Пожалуйста, отправь число или команду /итог");
-      }
-    }
-    res.status(200).send("OK")
-  } else {
-      res.setHeader('Allow', ['POST']);
-      res.status(500).send('Method Not Allowed');
-  }
+	if (req.method == "POST") {
+		const chatId = req.body.message.chat.id;
+		const text = req.body.message.text?.trim();
+		console.log("ChatID", chatId);
+		console.log("text", text);
+
+		if (!text) {
+			res.status(200).send("OK");
+			return;
+		}
+
+		if (text.startsWith("/start") || text.startsWith("/help")) {
+			await helpCommand(chatId);
+		} else if (text === "/total") {
+			await summaryCommandHandler(chatId);
+		} else {
+			// Попробуем обработать как число
+			const numberAdded = await addToSum(chatId, text);
+			if (!numberAdded) {
+				await sendMessage(
+					chatId,
+					"Пожалуйста, отправь число или команду /total"
+				);
+			}
+		}
+		res.status(200).send("OK");
+	} else {
+		res.setHeader("Allow", ["POST"]);
+		res.status(500).send("Method Not Allowed");
+	}
 }
